5a433d8ed9178d261b07a18afab3c87ddb48bedb
yingshaoxo <yingshaoxo@gmail.com>
2018-06-06 13:08:04 +0800

    update

--- mouse_control.py
+++ mouse_control.py
@@ -9,6 +9,8 @@ DISTANCE = 20
 
 mouseControl = MouseController()
 
+ctrl_status = 0
+
 
 def on_press(key):
     try:
@@ -16,6 +18,28 @@ def on_press(key):
     except:
         k = key.name
     print(k)
+
+    global ctrl_status
+    if k == "ctrl":
+        ctrl_status = 1
+
+    if ctrl_status == 1:
+        handle(k)
+
+
+def on_release(key):
+    try:
+        k = key.char
+    except:
+        k = key.name
+    print(k)
+
+    global ctrl_status
+    if k == "ctrl":
+        ctrl_status = 0
+
+
+def handle(k):
     if k == "up":
         mouseControl.move(0, -DISTANCE)
     elif k == "down":
@@ -24,10 +48,12 @@ def on_press(key):
         mouseControl.move(-DISTANCE, 0)
     elif k == "right":
         mouseControl.move(DISTANCE, 0)
+    elif k == "enter":
+        mouseControl.click(Button.left)
     elif k == "num_lock":
         mouseControl.click(Button.left)
 
 
-keyListen = KeyboardListener(on_press=on_press)
+keyListen = KeyboardListener(on_press=on_press, on_release=on_release)
 keyListen.start()
 keyListen.join()
